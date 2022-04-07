953b328138ba8180196132a07c9dff1d982e72f5
Kevin McCarthy <mccarthyk@hcflgov.net>
2022-04-07 09:20:02 -0400

    update docs

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -13,13 +13,14 @@ export * from './features'
  * This Vue 3 component can be found at src/components/Search/Bootstrap.vue
  *
  * ```html
- * <SearchBootstrap large hc-sources />
+ * <SearchBootstrap large hc-sources @results="myResultsHandler" />
  * ```
  *
- * The following "params" are optional [props](https://vuejs.org/guide/components/props.html#props) to be passed to the component:
- * @small adds the 'input-group-sm' class to the [input group](https://getbootstrap.com/docs/5.1/forms/input-group/#sizing)
- * @large adds the 'input-group-lg' class to the [input group](https://getbootstrap.com/docs/5.1/forms/input-group/#sizing)
- * @hcSources sets the search widget's sources to use a county locator for addresses and a parcel layer for folio numbers
+ * @@results the [event](https://vuejs.org/guide/components/events.html#component-events) emitted by the component when the form is submitted and {@link search} is resolved, returns `__esri.SearchResult[] | undefined` as the first argument
+ *
+ * @hcSources optional [prop](https://vuejs.org/guide/components/props.html#props), sets the search widget's sources to use a county locator for addresses and a parcel layer for folio numbers
+ * @small optional [prop](https://vuejs.org/guide/components/props.html#props), adds the 'input-group-sm' class to the [input group](https://getbootstrap.com/docs/5.1/forms/input-group/#sizing)
+ * @large optional [prop](https://vuejs.org/guide/components/props.html#props), adds the 'input-group-lg' class to the [input group](https://getbootstrap.com/docs/5.1/forms/input-group/#sizing)
  *
  * @category Components
  */
